fa0f4877c699035bc4d37a39ca9df86a876b29df
Leonardo da Silva Neves <leojutsu@gmail.com>
2026-02-03 10:29:15 -0300

    upgrades

--- SQL/cap06/Exercicio6-Cap06.sql
+++ SQL/cap06/Exercicio6-Cap06.sql
@@ -29,6 +29,35 @@ INSERT INTO cap06.vendas_loja_online (id_cliente, pais_cliente, visitas_ultimo_m
 
 
 
+-- Solução
 
+CREATE TABLE cap06.vendas_loja_online_final AS
+
+
+SELECT
+
+    id_cliente,
+
+    CASE WHEN pais_cliente = 'Brasil' THEN 1 ELSE 0 END AS pais_brasil,
+
+    CASE WHEN pais_cliente = 'Canadá' THEN 1 ELSE 0 END AS pais_canada,
+
+    CASE WHEN pais_cliente = 'Inglaterra' THEN 1 ELSE 0 END AS pais_inglaterra,
+
+    CASE WHEN visitas_ultimo_mes = 'sim' THEN 1 ELSE 0 END AS visitas_num,
+
+    CASE
+        WHEN compras_ultimo_mes = '0-5' THEN 1
+        WHEN compras_ultimo_mes = '6-10' THEN 2
+        WHEN compras_ultimo_mes = '11-15' THEN 3
+        WHEN compras_ultimo_mes = '16-20' THEN  4
+        ELSE 0
+    END AS compras_num,
+
+    CASE WHEN fez_compra_mes_atual = TRUE THEN 1 ELSE 0 END AS fez_compras_num,
+
+    total_gasto_ultimo_mes
+
+FROM cap06.vendas_loja_online;
 
 
